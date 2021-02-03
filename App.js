b890cbb5c53a97c808633b2db48a356ba5a3f0a8
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-02-02 19:36:36 -0600

    Ultima

--- App.js
+++ App.js
@@ -2,8 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Button} from 'react-native-elements';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+
 
 export default function App() {
   return (
