6a4b35f0aba216fd91a15898afd77c0e11c1dc45
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-02-03 17:03:16 -0600

    Nuevas ventanas

--- App.js
+++ App.js
@@ -7,10 +7,16 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import SigninScreen from './src/screens/Signin_screen';
 
+import MainScreen from './src/screens/main_screen';
+import ListScreen from './src/screens/List_screen';
+import CalculatorScreen from './src/screens/calculator_screen';
+
 const switchNavigator = createSwitchNavigator({
     loginFlow: createStackNavigator({
-      Signin: SigninScreen
+      Signin: SigninScreen,
+
     })
+
 });
 
 export default createAppContainer(switchNavigator);
