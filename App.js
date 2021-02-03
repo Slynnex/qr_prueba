fb0f45e71fc06908b3622935c9abe02b29810aaa
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-02-02 19:39:20 -0600

    libreria

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Button} from 'react-native-elements';
-
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 export default function App() {
   return (
