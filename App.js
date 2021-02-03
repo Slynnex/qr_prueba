73fbeb96bafe86d66ac36906e593f8ddfacb8808
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-02-03 16:40:39 -0600

    Ventana de signin funcionando

--- App.js
+++ App.js
@@ -2,9 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Button} from 'react-native-elements';
-import { createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import SigninScreen from './src/screens/Signin_screen';
 
 const switchNavigator = createSwitchNavigator({
-
+    loginFlow: createStackNavigator({
+      Signin: SigninScreen
+    })
 });
+
+export default createAppContainer(switchNavigator);
---
+++ src/screens/Signin_screen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const Signin_screen = () => {
+    return (
+        <View>
+            <Text>Signin screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Signin_screen;
