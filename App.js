7f4bc2cc5b538545dccde8738d13a448f15d72ba
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-02-03 16:33:15 -0600

    components

--- App.js
+++ App.js
@@ -2,23 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Button} from 'react-native-elements';
+import { createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Lalo se la come</Text>
-      <Button title="Si"/>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const switchNavigator = createSwitchNavigator({
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
