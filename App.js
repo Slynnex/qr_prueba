f77ea5dcbcabbba8329f69df81e75319838e4714
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-02-02 18:29:34 -0600

    React navigation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Button} from 'react-native-elements';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 export default function App() {
   return (
