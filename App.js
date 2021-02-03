24a09ab9c7cee1b9a70371808b46e8a31e30a39c
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-02-02 18:22:10 -0600

    Boton agregado

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Button} from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Lalo se la come</Text>
+      <Button title="Si"/>
       <StatusBar style="auto" />
     </View>
   );
