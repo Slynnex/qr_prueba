a2b0d9e24256ce2e8b640b8f7b18600512c7d28f
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-02-03 17:56:31 -0600

    Nuevas cosas

---
+++ src/components/AuthForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Text, Button, Input} from 'react-native-elements';
+
+const AuthForm = ({ headerText, errorMessage}) = {
+
+}
+
+const styles = StyleSheet.create({});
+
+export default AuthForm;
