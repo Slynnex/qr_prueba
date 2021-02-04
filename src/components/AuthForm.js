67c6e19c586953b6984508ca69ea4eea0a2812e5
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-02-03 18:17:46 -0600

    Singin y Singup componentes

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {View ,StyleSheet} from 'react-native';
 import {Text, Button, Input} from 'react-native-elements';
+import Spacer from './Spacer';
 
-const AuthForm = ({ headerText, errorMessage}) = {
-
+const AuthForm = ({ headerText, errorMessage}) => {
+    return (
+        <>
+            <Spacer>
+                <Text h3>{headerText}</Text>
+            </Spacer>
+        </>
+    )
 }
 
 const styles = StyleSheet.create({});
--- src/screens/Signin_screen.js
+++ src/screens/Signin_screen.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import {View, StyleSheet, Text, Button} from 'react-native';
+import AuthForm from '../components/AuthForm';
+import {NavigationEvents} from 'react-navigation';
 
 const Signin_screen = ({ navigation }) => {
     return (
-        <View>
-            <Text>Signin screen</Text>
+        <View style={styles.container}>
+            <AuthForm 
+                headerText="Iniciar sesión"
+                errorMessage="Datos incorrectos"
+            />
             <Button title="Signup" onPress={() => navigation.navigate("Qr")}/>
             <Button title="QrS" onPress={() => navigation.navigate("Qr")}/>
         </View>
     );
 }
 
-const styles = StyleSheet.create({
+Signin_screen.navigationOptions = () => {
+    return {
+    header: () => false
+    };
+}
 
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: 'center',
+        marginBottom: 250
+    }
 });
 
 export default Signin_screen;
