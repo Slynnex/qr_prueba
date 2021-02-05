ab0e5f0aaa2f8e9922f9ddaeceafb5dc76b5b9cc
sergioreag <sergioreag@hotmail.com>
2021-02-05 17:38:08 -0600

    mainform changes

--- src/components/mainForm.js
+++ src/components/mainForm.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import {View ,StyleSheet} from 'react-native';
-import {Text, Button, Input} from 'react-native-elements';
+import {View ,StyleSheet, Image, Button} from 'react-native';
+import {Text, Input} from 'react-native-elements';
 import Spacer from './Spacer';
 
 const MainForm = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <Spacer>
-                <Text>hola mundo</Text>
+                <Text style={styles.titulo}>N conteo</Text>
                 
+                <View>
+                    <Text style={styles.titulo}>Nombre</Text>
+                    <Text style={styles.titulo}>Fecha</Text>
+                </View>
+               
+                <Image 
+                    source={{ uri: 'https://assets.geekmi.news/__export/1607114141528/sites/debate/img/2020/12/04/como-desbloquear-en-genshin-impact-la-mision-de-la-historia-de-mona_crop1607114115975.jpg_988992781.jpg' }}
+                    style={ styles.imagen }
+                />
+
+                <Text style={styles.titulo}>codigo</Text>
+                <Button title= "QR"/>
             </Spacer>
         </View>
     );
@@ -18,9 +30,17 @@ const styles = StyleSheet.create({
 
     container: {
         flex: 1,
-        alignItems: 'center',
+        
 
         },
+    imagen:{
+        width: 200, 
+        height: 200,
+    },
+    titulo:{
+    justifyContent: 'center',
+    fontSize:20
+    }
 
 });
 
