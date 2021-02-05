d58ca8c67415387a583902383bdaf731c983b505
sergioreag <sergioreag@hotmail.com>
2021-02-05 16:42:40 -0600

    mianfform creado

---
+++ src/components/mainForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View ,StyleSheet} from 'react-native';
+import {Text, Button, Input} from 'react-native-elements';
+import Spacer from './Spacer';
+
+const mainForm = () => {
+    return (
+        <>
+            <Spacer>
+                <Text h3>hola mundo</Text>
+            </Spacer>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({});
+
+export default mainForm;
