4585b7b4472178e4e8d4999b515a1f49c689e16a
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-02-06 16:35:37 -0600

    Para que ya no chingue esta pendejada

--- src/screens/main_screen.js
+++ src/screens/main_screen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
-import MainForm from '../components/MainForm';
+import MainForm from '../components/mainForm';
 
 const Main_screen = () => {
     return (
