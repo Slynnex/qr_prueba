71b2b8a5b5b0606c2f971c16666d644bf408c102
sergioreag <sergioreag@hotmail.com>
2021-02-05 18:53:38 -0600

    mainform

--- src/components/mainForm.js
+++ src/components/mainForm.js
@@ -5,42 +5,80 @@ import Spacer from './Spacer';
 
 const MainForm = () => {
     return (
+        <>
         <View style={styles.container}>
             
-                <Text style={styles.titulo}>N dfgdfgdf</Text>
+                <Text style={styles.titulo}>no articulo</Text>
                 
-                <View>
-                    <Text style={styles.titulo}>Nombre</Text>
-                    <Text style={styles.titulo}>Fecha</Text>
+                <View style={styles.datos}>
+                    <Text style={styles.letras}>Nombre</Text>
+                    <Text style={styles.letras}>Fecha</Text>
                 </View>
-               
-                <Image 
+               <View style={styles.subcontainer}>
+               <Image 
                     source={{ uri: 'https://assets.geekmi.news/__export/1607114141528/sites/debate/img/2020/12/04/como-desbloquear-en-genshin-impact-la-mision-de-la-historia-de-mona_crop1607114115975.jpg_988992781.jpg' }}
                     style={ styles.imagen }
                 />
 
-                <Text style={styles.titulo}>codigo</Text>
-                <Button title= "QR"/>
+                <Text style={styles.letras}>codigo:</Text>
+                <Input></Input>
+                <Button style={styles.boton} title= "QR"/>
+                <Text style={styles.letras}>ubicación</Text>
+                <Input></Input>
+                <Button style={styles.boton} title= "Enviar"/>
+               </View>
+               
            
         </View>
+        </>
     );
 }
 
 const styles = StyleSheet.create({
 
     container: {
-        flex: 1,
         
-
+        marginVertical: 10,
+        marginHorizontal: 10,
+        top:10,
+        bottom:0,
+        left:0,
+        right:0,
+        //borderWidth:3,
+        //borderColor: 'red',
         },
     imagen:{
+        
         width: 200, 
         height: 200,
+        marginBottom:30,
+        
+    },
+    datos:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom:10,
     },
     titulo:{
-    justifyContent: 'center',
-    fontSize:20
-    }
+    fontWeight: 'bold',
+    textAlign:'center',
+    fontSize:20,
+    marginBottom:10,
+    },
+    boton:{
+        width: 50, 
+        height: 50
+    },
+    subcontainer:{
+        alignItems: "center",
+        //borderWidth:3,
+        //borderColor: 'red',
+    },
+    letras:{
+        
+        textAlign:'center',
+        fontSize:20
+    },
 
 });
 
